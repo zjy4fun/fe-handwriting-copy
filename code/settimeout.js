c9e1863dd56fcfc1b5a21f93efe0ba78ae2e2639
zjy4fun <zjy4fun@gmail.com>
2022-12-16 14:59:20 +0800

    补充 setTimeout 实现 setInterval 的原因和实现方式

--- code/settimeout.js
+++ code/settimeout.js
@@ -1,8 +1,13 @@
 /**
  * settimeout 模拟实现 setinterval(带清除定时器的版本)
  * 题目描述:setinterval 用来实现循环定时调用 可能会存在一定的问题 能用 settimeout 解决吗
+ *
+ * 应用场景：
+ * 在事件循环中，异步任务会进入任务队列中执行，像 setTimeout 和 setInterval 这种异步代码，会将里面的回调函数放入 WebAPI 提供的单独运行空间中，等待延迟时间以后放入任务队列
+ * 不同的是，setTimeout 延迟时间以后直接放入任务队列，而 setInterval 会检查任务队列是否有上次未执行的异步任务，如果没有才会进入任务队列
+ * 这就导致如果 setInterval 上次任务的执行仍在任务队列中，就会导致 setInterval 没有按照期待的时间间隔执行
  */
-function mySetTimeout(fn, t) {
+function myInterval(fn, t) {
     let timer = null
     function interval() {
         fn()
@@ -16,9 +21,9 @@ function mySetTimeout(fn, t) {
     }
 }
 
-let a = mySetTimeout(() => {
+let myIntervalA = myInterval(() => {
     console.log(111)
 }, 1000)
-let b = mySetTimeout(() => {
+let myIntervalB = myInterval(() => {
     console.log(222)
 }, 1000)
