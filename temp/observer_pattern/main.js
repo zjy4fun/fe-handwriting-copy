d4751876fa00937420ba545419545cdae3aa7cde
zjy4fun <zjy4fun@gmail.com>
2022-12-20 00:05:21 +0800

    add comments about observer pattern

--- temp/observer_pattern/main.js
+++ temp/observer_pattern/main.js
@@ -2,10 +2,16 @@ const Subject = require('./Subject')
 const Observer = require('./Observer')
 
 
-const subject = new Subject()
-const observerA = new Observer('observerA', subject)
-const observerB = new Observer('observerB')
-subject.addObserver(observerB)
+const subject = new Subject() //被观察者
+const observerA = new Observer('observerA', subject)//观察者A，申请加入被观察者
+const observerB = new Observer('observerB')//观察者B
+subject.addObserver(observerB)//被观察者主动加入观察者B
 subject.notifyObservers('Hello from subject')
+
 subject.removeObserver(observerA)
 subject.notifyObservers('Hello again')
+/**
+    observerA got message Hello from subject
+    observerB got message Hello from subject
+    observerB got message Hello again
+ */
