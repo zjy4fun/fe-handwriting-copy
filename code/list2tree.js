cd8342dda929d59301e0682bda58fb407aebd906
zjy4fun <zjy4fun@gmail.com>
2022-12-31 22:10:09 +0800

    列表转成树形结构

---
+++ code/list2tree.js
@@ -0,0 +1,21 @@
+/**
+ * 列表转成树形结构
+ */
+function list2tree(data) {
+    let temp = {}
+    let treeData = []
+    for(let i = 0; i < data.length; i++) {
+        temp[data[i].id] = data[i]
+    }
+    for(let i in temp) {
+        if(+temp[i].parentId !== 0){
+            if(!temp[temp[i].parentId].children){
+                temp[temp[i].parentId].children = []
+            }
+            temp[temp[i].parentId].children.push(temp[i])
+        }else{
+            treeData.push(temp[i])
+        }
+    }
+    return treeData
+}
