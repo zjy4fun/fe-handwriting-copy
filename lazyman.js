2d56dd81a540ae8d9c492cee4e05eac499c922dd
zjy4fun <zjy4fun@gmail.com>
2022-12-31 18:40:04 +0800

    实现 LazyMan fix https://github.com/zjy4fun/fe-handwriting-copy/issues/21

---
+++ lazyman.js
@@ -0,0 +1,54 @@
+/**
+ * 实现 LazyMan
+ *
+ */
+class _LazyMan {
+    constructor(name) {
+        this.tasks = []
+        const task = () => {
+            console.log(`Hi! This is ${name}`)
+            this.next();
+        };
+        this.tasks.push(task)
+        setTimeout(() => {
+            //把 this.next() 放到调用栈清空之后执行
+            this.next()
+        }, 0)
+    }
+    next() {
+        const task = this.tasks.shift()
+        task && task()
+    }
+    sleep(time) {
+        this._sleepWrapper(time, false)
+        return this;
+    }
+    sleepFirst(time) {
+        this._sleepWrapper(time, true)
+        return this
+    }
+    _sleepWrapper(time, first) {
+        const task = () => {
+            setTimeout(() => {
+                console.log(`Wake up after ${time}`)
+                this.next();
+            }, time * 1000);
+        };
+        if(first) {
+            this.tasks.unshift(task)//放到任务队列顶部
+        }else{
+            this.tasks.push(task)//放到任务队列尾部
+        }
+    }
+    eat(name) {
+        const task = () => {
+            console.log(`Eat ${name}`)
+            this.next()
+        };
+        this.tasks.push(task)
+        return this
+    }
+}
+function LazyMan(name) {
+    return new _LazyMan(name)
+}
