cb6decb82e1f5fff58e7e3e345d11397f3f76c51
zjy4fun <zjy4fun@gmail.com>
2022-12-31 21:42:35 +0800

    实现AJAX

---
+++ code/ajax.js
@@ -0,0 +1,23 @@
+/**
+ * 利用 XMLHttpRequest 手写 AJAX 实现
+ */
+const getJSON = function(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open("GET", url, false)
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState !== 4) return;
+            if(xhr.status === 200 || xhr.status === 304) {
+                resolve(xhr.responseText)
+            }else{
+                reject(new Error(xhr.responseText))
+            }
+        };
+        xhr.send()
+    })
+}
+
+getJSON("https://baidu.com").then((res) => {
+    console.log(res)
+})
